e4c525e0a7f7d24430062523ec96f8778fd2e408
cristianpela <cristypela05@gmail.com>
2018-10-31 13:40:54 +0200

    android studio project setup

---
+++ app/src/main/java/com/crskdev/biblereaderplus/MainActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * Created by Pela Cristian on 31.10.2018 13:40.
+ * License: MIT
+ * Copyright (c) 2018.
+ */
+package com.crskdev.biblereaderplus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/crskdev/biblereaderplus/ExampleUnitTest.kt
@@ -0,0 +1,22 @@
+/*
+ * Created by Pela Cristian on 31.10.2018 13:40.
+ * License: MIT
+ * Copyright (c) 2018.
+ */
+
+package com.crskdev.biblereaderplus
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
