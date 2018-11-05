c443bd40da3ab9ba87185ec7576c2df9e907403d
cristianpela <cristypela05@gmail.com>
2018-11-05 11:45:25 +0200

    added application class

---
+++ app/src/main/java/com/crskdev/biblereaderplus/BibleReaderApplication.kt
@@ -0,0 +1,14 @@
+/*
+ * License: MIT
+ * Copyright (c)  Pela Cristian 2018.
+ */
+
+package com.crskdev.biblereaderplus
+
+import android.app.Application
+
+/**
+ * Created by Cristian Pela on 05.11.2018.
+ */
+class BibleReaderApplication : Application() {
+}
