b9361b51ed371e5c1bed8aeb2d3ff7b0868a057e
cristianpela <cristypela05@gmail.com>
2018-11-05 12:46:26 +0200

    fragment dagger hookup

---
+++ app/src/main/java/com/crskdev/biblereaderplus/common/util/extensions.kt
@@ -0,0 +1,24 @@
+/*
+ * License: MIT
+ * Copyright (c)  Pela Cristian 2018.
+ */
+
+package com.crskdev.biblereaderplus.common.util
+
+/**
+ * Created by Cristian Pela on 31.10.2018.
+ */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Any.cast(): T = this as T
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> Any.castIf(): T? =
+    try {
+        this as T
+    } catch (ex: Exception) {
+        null
+    }
+
+infix fun Any?.ifNull(block: () -> Unit) {
+    if (this == null) block()
+}
