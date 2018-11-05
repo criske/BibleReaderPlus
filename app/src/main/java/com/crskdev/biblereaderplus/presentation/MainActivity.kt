0d7c0944e7a0fd76885c4ce3e9810d42ed433df3
cristianpela <cristypela05@gmail.com>
2018-11-05 15:33:11 +0200

    adding nav dynamically

--- app/src/main/java/com/crskdev/biblereaderplus/presentation/MainActivity.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/presentation/MainActivity.kt
@@ -5,7 +5,9 @@
 package com.crskdev.biblereaderplus.presentation
 
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.crskdev.biblereaderplus.R
+import com.crskdev.biblereaderplus.presentation.util.arch.dynamicallyLoadNavGraph
 import dagger.android.support.DaggerAppCompatActivity
 
 
@@ -14,5 +16,15 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dynamicallyLoadNavGraph(
+            R.id.container,
+            R.id.setupFragment,
+            R.navigation.main_nav_graph,
+            supportFragmentManager,
+            savedInstanceState
+        )
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.container).navigateUp()
+
 }
