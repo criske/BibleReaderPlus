425c8ac00b203fde4ed63f28731d65088309c3bd
cristianpela <cristypela05@gmail.com>
2018-11-08 14:15:55 +0200

    scaffold read interactor

--- app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/ReadInteractor.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/ReadInteractor.kt
@@ -6,11 +6,24 @@
 package com.crskdev.biblereaderplus.domain.interactors.read
 
 import com.crskdev.biblereaderplus.domain.gateway.DocumentRepository
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 /**
  * Created by Cristian Pela on 08.11.2018.
  */
 class ReadInteractor @Inject constructor(
+
     private val documentRepository: DocumentRepository) {
+
+
+    suspend fun request(request: Request) = coroutineScope {
+
+    }
+
+
+    class Response
+
+    class Request(val responseChannel: Channel<Response>)
 }
