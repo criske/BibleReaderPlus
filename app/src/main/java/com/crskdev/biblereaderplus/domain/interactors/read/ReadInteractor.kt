672ed5615b0d8c902ab0cef69f3cb53aebf585b7
cristianpela <cristypela05@gmail.com>
2018-11-13 16:51:09 +0200

    tags and favorites scaffold

--- app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/ReadInteractor.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/ReadInteractor.kt
@@ -10,29 +10,20 @@ package com.crskdev.biblereaderplus.domain.interactors.read
 import androidx.paging.PagedList
 import com.crskdev.biblereaderplus.domain.entity.Read
 import com.crskdev.biblereaderplus.domain.gateway.DocumentRepository
-import com.crskdev.biblereaderplus.domain.gateway.GatewayDispatchers
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
  * Created by Cristian Pela on 08.11.2018.
  */
 class ReadInteractor @Inject constructor(
-    private val dispatchers: GatewayDispatchers,
     private val documentRepository: DocumentRepository) {
-
+    
     suspend fun request(request: Request) = coroutineScope {
-        request.responseChannel.invokeOnClose {
-            throw it ?: Error("Unknown closed exception")
-        }
-        documentRepository.read {
-            launch(dispatchers.MAIN) {
-                request.responseChannel.send(Response.Paged(it))
-            }
-        }
+
     }
+
 
     sealed class Response {
         class Paged(val list: PagedList<Read>) : Response()
