c5dfa21bb1eda1c8ae630adfe3414ad474d3cb01
cristianpela <cristypela05@gmail.com>
2018-11-14 13:12:48 +0200

    content interactor impl started

---
+++ app/src/test/java/com/crskdev/biblereaderplus/domain/interactors/read/ContentInteractorImplTest.kt
@@ -0,0 +1,15 @@
+package com.crskdev.biblereaderplus.domain.interactors.read
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Cristian Pela on 14.11.2018.
+ */
+class ContentInteractorImplTest {
+
+    @Test
+    fun request() {
+    }
+}
