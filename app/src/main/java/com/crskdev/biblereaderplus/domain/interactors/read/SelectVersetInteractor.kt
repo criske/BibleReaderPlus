8430dec18d7c45af2fcc8ce069be8955acecf252
cristianpela <cristypela05@gmail.com>
2018-11-16 17:36:11 +0200

    select verset interactor fully tested better retry a launchIgnorThrow scope based on supervisorScope

--- app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/SelectVersetInteractor.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/read/SelectVersetInteractor.kt
@@ -1,24 +1,30 @@
 package com.crskdev.biblereaderplus.domain.interactors.read
 
+import com.crskdev.biblereaderplus.common.util.launchIgnoreThrow
+import com.crskdev.biblereaderplus.common.util.retry
+import com.crskdev.biblereaderplus.common.util.sendAndClose
 import com.crskdev.biblereaderplus.domain.entity.SelectedVerset
 import com.crskdev.biblereaderplus.domain.entity.VersetKey
 import com.crskdev.biblereaderplus.domain.gateway.DocumentRepository
 import com.crskdev.biblereaderplus.domain.gateway.GatewayDispatchers
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.actor
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by Cristian Pela on 13.11.2018.
  */
+@Suppress("unused")
 interface SelectVersetInteractor {
 
     suspend fun request(versetKey: VersetKey, response: (Response) -> Unit)
 
     sealed class Response {
         class OK(val result: SelectedVerset) : Response()
+        class Partial(val result: SelectedVerset, val throwable: Throwable) : Response()
         object Wait : Response()
         sealed class Error : Response() {
-            class Generic(val throwable: Throwable) : Error()
+            class GenericErr(val throwable: Throwable) : Error()
             object NotFound : Error()
         }
     }
@@ -32,7 +38,6 @@ class SelectVersetInteractorImpl(
 ) : SelectVersetInteractor {
 
     override suspend fun request(versetKey: VersetKey, response: (SelectVersetInteractor.Response) -> Unit) =
-
         coroutineScope {
 
             val sender = actor<SelectVersetInteractor.Response> {
@@ -40,43 +45,47 @@ class SelectVersetInteractorImpl(
                     response(r)
                 }
             }
-
             sender.send(SelectVersetInteractor.Response.Wait)
 
-            supervisorScope {
+            val handler: suspend (CoroutineContext, Throwable) -> Unit = { ctx, err ->
+                sender.sendAndClose(
+                    when (err) {
+                        is ErrorResponseAdapter -> err.errorResponse
+                        else -> SelectVersetInteractor.Response.Error.GenericErr(err)
+                    }
+                )
+            }
 
-                val errHandler = coroutineContext + CoroutineExceptionHandler { ctx, throwable ->
-                    runBlocking(ctx + dispatchers.MAIN) {
-                        when (throwable) {
-                            is ErrorResponseAdapter -> sender.send(throwable.errorResponse)
-                            else -> sender.send(SelectVersetInteractor.Response.Error.Generic(throwable))
+            launchIgnoreThrow(handler = handler) {
+                //local
+                val verset = withContext(coroutineContext + dispatchers.DEFAULT) {
+                    repository.getVerset(versetKey)
+                        ?: throw ErrorResponseAdapter(SelectVersetInteractor.Response.Error.NotFound)
+                }
+
+                //remote
+                val firstErrorTracker: (Int, Throwable) -> Unit = { i, ex ->
+                    if (i == 1) {//on first retry, send a partial result based on the local result
+                        launch {
+                            sender.send(SelectVersetInteractor.Response.Partial(verset, ex))
                         }
                     }
-                    sender.close()
                 }
-
-                launch(errHandler + dispatchers.DEFAULT) {
-
-                    val verset = repository.getVerset(versetKey)
-                        ?: throw ErrorResponseAdapter(SelectVersetInteractor.Response.Error.NotFound)
-
-                    val props = withContext(dispatchers.IO) {
+                val props = retry(3, tracker = firstErrorTracker) {
+                    withContext(coroutineContext + dispatchers.IO) {
                         repository.getVersetProps(versetKey)
                     }
-
-                    with(sender) {
-                        send(
-                            SelectVersetInteractor.Response.OK(
-                                verset.copy(isFavorite = props.isFavorite, tags = props.tags)
-                            )
-                        )
-                        close()
-                        Unit
-                    }
                 }
+
+                sender.sendAndClose(
+                    SelectVersetInteractor.Response.OK(
+                        verset.copy(isFavorite = props.isFavorite, tags = props.tags)
+                    )
+                )
                 Unit
             }
             Unit
+
         }
 
     private class ErrorResponseAdapter(val errorResponse: SelectVersetInteractor.Response.Error) :
