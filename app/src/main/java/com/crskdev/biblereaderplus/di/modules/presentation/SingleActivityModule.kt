a8f4438610a441c09770fbac96e7b597e5bdd395
cristianpela <cristypela05@gmail.com>
2018-11-21 15:57:02 +0200

    basic integration with interactor works

--- app/src/main/java/com/crskdev/biblereaderplus/di/modules/presentation/SingleActivityModule.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/di/modules/presentation/SingleActivityModule.kt
@@ -5,36 +5,21 @@
 
 package com.crskdev.biblereaderplus.di.modules.presentation
 
-import android.content.Context
-import com.crskdev.biblereaderplus.R
-import com.crskdev.biblereaderplus.common.util.enumMap
-import com.crskdev.biblereaderplus.di.modules.presentation.favorite.FavoriteVersetDetailModule
 import com.crskdev.biblereaderplus.di.modules.presentation.favorite.FavoriteVersetsModule
 import com.crskdev.biblereaderplus.di.modules.presentation.read.ReadModule
 import com.crskdev.biblereaderplus.di.modules.presentation.setup.SetupModule
-import com.crskdev.biblereaderplus.di.scopes.PerActivity
 import com.crskdev.biblereaderplus.di.scopes.PerFragment
-import com.crskdev.biblereaderplus.presentation.common.CharSequenceTransformerFactory
-import com.crskdev.biblereaderplus.presentation.common.HighLightContentTransformer
-import com.crskdev.biblereaderplus.presentation.common.IconAtEndTransformer
-import com.crskdev.biblereaderplus.presentation.common.LeadFirstLineTransformer
-import com.crskdev.biblereaderplus.presentation.favorite.FavoriteVersetDetailFragment
 import com.crskdev.biblereaderplus.presentation.favorite.FavoriteVersetsFragment
 import com.crskdev.biblereaderplus.presentation.read.ReadFragment
 import com.crskdev.biblereaderplus.presentation.setup.SetupFragment
 import dagger.Module
-import dagger.Provides
 import dagger.android.ContributesAndroidInjector
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 
 /**
  * Created by Cristian Pela on 05.11.2018.
  */
 
 @Module
-@ObsoleteCoroutinesApi
-@ExperimentalCoroutinesApi
 abstract class SingleActivityModule {
 
     @PerFragment
@@ -48,31 +33,6 @@ abstract class SingleActivityModule {
     @PerFragment
     @ContributesAndroidInjector(modules = [FavoriteVersetsModule::class])
     abstract fun favoriteVersetsFragmentInjector(): FavoriteVersetsFragment
-
-    @PerFragment
-    @ContributesAndroidInjector(modules = [FavoriteVersetDetailModule::class])
-    abstract fun favoriteVersetDetailFragmentInjector(): FavoriteVersetDetailFragment
-
-    @Module
-    companion object {
-        @JvmStatic
-        @PerActivity
-        @Provides
-        fun provideSpanTransformers(context: Context): CharSequenceTransformerFactory =
-            CharSequenceTransformerFactory(
-                enumMap {
-                    this[CharSequenceTransformerFactory.Type.ICON_AT_END] = lazy {
-                        IconAtEndTransformer(context, R.drawable.ic_star_black_10dp)
-                    }
-                    this[CharSequenceTransformerFactory.Type.LEAD_FIRST_LINE] = lazy {
-                        LeadFirstLineTransformer()
-                    }
-                    this[CharSequenceTransformerFactory.Type.HIGHLIGHT] = lazy {
-                        HighLightContentTransformer()
-                    }
-                }
-            )
-    }
 
 }
 
--- app/src/main/java/com/crskdev/biblereaderplus/di/modules/presentation/favorite/test.kt
+++
@@ -1,5 +0,0 @@
-package com.crskdev.biblereaderplus.di.modules.presentation.favorite
-
-/**
- * Created by Cristian Pela on 21.11.2018.
- */
