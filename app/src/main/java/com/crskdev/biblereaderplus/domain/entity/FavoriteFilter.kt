0123c9166c9b99a8e08af8b3d4c4d06036f83635
cristianpela <cristypela05@gmail.com>
2018-11-27 15:00:41 +0200

    multi tag for search by tag

--- app/src/main/java/com/crskdev/biblereaderplus/domain/entity/FavoriteFilter.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/domain/entity/FavoriteFilter.kt
@@ -8,5 +8,5 @@ package com.crskdev.biblereaderplus.domain.entity
 sealed class FavoriteFilter(val asc: Boolean) {
     class None(asc: Boolean = false) : FavoriteFilter(asc)
     class Query(val query: String, asc: Boolean = false) : FavoriteFilter(asc)
-    class ByTag(val tag: Tag, asc: Boolean = false) : FavoriteFilter(asc)
+    class ByTag(val tags: List<Tag>, asc: Boolean = false) : FavoriteFilter(asc)
 }
