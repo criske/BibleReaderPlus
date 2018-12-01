30d97b8922ea804ccc17305aed4484b8d0ebad65
cristianpela <cristypela05@gmail.com>
2018-12-01 17:10:33 +0200

    refactor fetch tags into a mocked interactor

--- app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/tag/FetchTagsInteractor.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/domain/interactors/tag/FetchTagsInteractor.kt
@@ -1,6 +1,17 @@
+/*
+ * License: MIT
+ * Copyright (c)  Pela Cristian 2018.
+ */
+
 package com.crskdev.biblereaderplus.domain.interactors.tag
+
+import com.crskdev.biblereaderplus.domain.entity.Tag
 
 /**
  * Created by Cristian Pela on 13.11.2018.
  */
-interface FetchTagsInteractor
+interface FetchTagsInteractor {
+
+    suspend fun request(query: String): List<Tag>
+
+}
