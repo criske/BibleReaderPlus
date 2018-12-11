81b9e4e307c8f457171881239448a1bf49357285
cristianpela <cristypela05@gmail.com>
2018-12-11 16:09:01 +0200

    mocked db

--- app/src/main/java/com/crskdev/biblereaderplus/data/DocumentRepositoryImpl.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/data/DocumentRepositoryImpl.kt
@@ -14,7 +14,9 @@ import com.crskdev.biblereaderplus.common.util.pagedlist.InMemoryPagedListDataSo
 import com.crskdev.biblereaderplus.domain.entity.*
 import com.crskdev.biblereaderplus.domain.gateway.DocumentRepository
 import com.crskdev.biblereaderplus.presentation.util.arch.filter
-import kotlinx.coroutines.Dispatchers
+import com.crskdev.biblereaderplus.presentation.util.arch.map
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -24,37 +26,70 @@ import java.util.*
 /**
  * Created by Cristian Pela on 21.11.2018.
  */
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
 class DocumentRepositoryImpl : DocumentRepository {
+
+    private data class Database(
+        val versets: List<Read.Verset>,
+        val tags: List<Tag>
+    )
 
     private var dataSource: DataSource<Int, Read.Verset>? = null
 
-    private val versetsLiveData = MutableLiveData<List<Read.Verset>>().apply {
-        value = Random().let { r ->
-            val wordLengths = listOf(5, 10, 3, 2, 7, 15, 12)
-            (1..100)
-                .map { id ->
-                    val paragraphLength = r.nextInt(10) + 50
-                    var wordsCountDown = paragraphLength
-                    val content = buildString {
-                        while (wordsCountDown > 0) {
-                            val delim = if (wordsCountDown > 0) " " else ""
-                            val word = sequence {
-                                while (true) {
-                                    yield((r.nextInt(24) + 97).toChar())
-                                }
-                            }.take(wordLengths.random()).joinToString("") + delim
-                            append(word)
-                            wordsCountDown--
+    private val dbLiveData: MutableLiveData<Database>
+
+    init {
+        dbLiveData = MutableLiveData<Database>().apply {
+            val versets = Random().let { r ->
+                val wordLengths = listOf(5, 10, 3, 2, 7, 15, 12)
+                (1..100)
+                    .map { id ->
+                        val paragraphLength = r.nextInt(10) + 50
+                        var wordsCountDown = paragraphLength
+                        val content = buildString {
+                            while (wordsCountDown > 0) {
+                                val delim = if (wordsCountDown > 0) " " else ""
+                                val word = sequence {
+                                    while (true) {
+                                        yield((r.nextInt(24) + 97).toChar())
+                                    }
+                                }.take(wordLengths.random()).joinToString("") + delim
+                                append(word)
+                                wordsCountDown--
+                            }
                         }
+                        Read.Verset(
+                            VersetKey(id, 1, 1, "remote$id"),
+                            id,
+                            content,
+                            r.nextBoolean(),
+                            ModifiedAt("")
+                        )
                     }
-                    Read.Verset(
-                        VersetKey(id, 1, 1, "remote$id"),
-                        id,
-                        content,
-                        r.nextBoolean(),
-                        ModifiedAt("")
-                    )
+            }
+
+            val tags = (listOf(
+                "#ffcc99",
+                "#cc66ff",
+                "#f7ffe6",
+                "#80aaff",
+                "#ff66a3",
+                "#ffffff",
+                "#000000"
+            ) to listOf(
+                "",
+                "a",
+                "abc",
+                "abcd",
+                "abdcdefg"
+            )).let {
+                val (colors, tagPrefixes) = it
+                (0..100).map {
+                    Tag(it + 1, "Tag${it + 1}${tagPrefixes.random()}", colors.random())
                 }
+            }
+            value = Database(versets, tags)
         }
     }
 
@@ -76,7 +111,7 @@ class DocumentRepositoryImpl : DocumentRepository {
     }
 
     override fun getVerset(versetKey: VersetKey): SelectedVerset? =
-        versetsLiveData.value?.firstOrNull { it.key == versetKey }?.let {
+        dbLiveData.value?.versets?.firstOrNull { it.key == versetKey }?.let {
             SelectedVerset(
                 it.key, "Book${it.key.bookId}", it.key.chapterId,
                 it.key.id,
@@ -85,33 +120,36 @@ class DocumentRepositoryImpl : DocumentRepository {
             )
         }
 
-    override suspend fun observeVerset(versetKey: VersetKey, observer: (SelectedVerset) -> Unit) = coroutineScope {
-        val actor = actor<Read.Verset> {
-            for (rv in channel) {
-                observer(rv.let {
-                    SelectedVerset(
-                        it.key, "Book${it.key.bookId}", it.key.chapterId,
-                        it.key.id,
-                        it.content,
-                        it.isFavorite
-                    )
-                })
+    override suspend fun observeVerset(versetKey: VersetKey, observer: (SelectedVerset) -> Unit) =
+        coroutineScope {
+            val actor = actor<Read.Verset> {
+                for (rv in channel) {
+                    observer(rv.let {
+                        SelectedVerset(
+                            it.key, "Book${it.key.bookId}", it.key.chapterId,
+                            it.key.id,
+                            it.content,
+                            it.isFavorite
+                        )
+                    })
+                }
             }
-        }
-        val liveDataObserver = Observer<List<Read.Verset>> {
-            launch(Dispatchers.Default) {
-                actor.send(it.first { it.key == versetKey })
+            val liveDataObserver = Observer<List<Read.Verset>> {
+                launch {
+                    actor.send(it.first { it.key == versetKey })
+                }
             }
-        }
-        versetsLiveData
-            .filter { it?.any { it.key == versetKey } ?: false }
-            .observeForever(liveDataObserver)
+            val versetsLiveData = dbLiveData
+                .filter { it?.versets?.any { it.key == versetKey } ?: false }
+                .map { it.versets }
 
-        actor.invokeOnClose {
-            versetsLiveData.removeObserver(liveDataObserver)
+            versetsLiveData.observeForever(liveDataObserver)
+
+            actor.invokeOnClose {
+                versetsLiveData.removeObserver(liveDataObserver)
+            }
+            Unit
         }
-        Unit
-    }
 
     override fun getVersetProps(versetKey: VersetKey): VersetProps {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -122,12 +160,14 @@ class DocumentRepositoryImpl : DocumentRepository {
     }
 
     override fun favoriteAction(versetKey: VersetKey, add: Boolean) {
-        versetsLiveData.postValue(versetsLiveData.value?.map {
-            if (it.key == versetKey) {
-                it.copy(isFavorite = add)
-            } else {
-                it
-            }
+        dbLiveData.postValue(dbLiveData.value?.let {
+            it.copy(versets = it.versets.map {
+                if (it.key == versetKey) {
+                    it.copy(isFavorite = add)
+                } else {
+                    it
+                }
+            })
         })
         sleep(200)//gib time for da value to settle
         dataSource?.invalidate()
@@ -135,12 +175,14 @@ class DocumentRepositoryImpl : DocumentRepository {
 
     @Synchronized
     override fun favorites(filter: FavoriteFilter): DataSource.Factory<Int, Read.Verset> {
-        versetsLiveData.value = versetsLiveData.value?.map {
-            it.copy(content = "$filter|->:${it.content}")
+        dbLiveData.value = dbLiveData.value?.let {
+            it.copy(versets = it.versets.map {
+                it.copy(content = "$filter|->:${it.content}")
+            })
         }
         return dataSourceFactory {
             InMemoryPagedListDataSource {
-                versetsLiveData.value ?: emptyList()
+                dbLiveData.value?.versets ?: emptyList()
             }.apply {
                 dataSource = this
             }
