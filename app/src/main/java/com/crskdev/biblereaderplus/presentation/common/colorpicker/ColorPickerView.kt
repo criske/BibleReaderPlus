1daea0532fcc3d54427e9b7cd5ac67f8975cee3b
cristianpela <cristypela05@gmail.com>
2018-12-17 20:42:02 +0200

    color picker most done

--- app/src/main/java/com/crskdev/biblereaderplus/presentation/common/colorpicker/ColorPickerView.kt
+++ app/src/main/java/com/crskdev/biblereaderplus/presentation/common/colorpicker/ColorPickerView.kt
@@ -7,86 +7,186 @@ package com.crskdev.biblereaderplus.presentation.common.colorpicker
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.SeekBar
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.core.graphics.ColorUtils
+import androidx.core.graphics.toColorFilter
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Observer
 import com.crskdev.biblereaderplus.R
+import com.crskdev.biblereaderplus.common.util.cast
 import com.crskdev.biblereaderplus.presentation.util.arch.ViewLifecycleOwner
-import com.crskdev.biblereaderplus.presentation.util.arch.map
-import com.crskdev.biblereaderplus.presentation.util.arch.scan
+import com.crskdev.biblereaderplus.presentation.util.system.getColorCompat
+import kotlinx.android.synthetic.main.color_picker_layout.view.*
 
 /**
- * Just simple color picker
+ * Just a simple color picker
  */
 class ColorPickerView : ConstraintLayout {
 
     private var colorPickListener: ((Int) -> Unit)? = null
 
-    private lateinit var viewModel: ColorPickerViewModel
+    private var viewModel: ColorPickerViewModel = ColorPickerViewModel()
 
     private val lifecycleOwner = ViewLifecycleOwner(this)
 
     constructor(context: Context) : this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        LayoutInflater.from(context).inflate(R.layout.color_picker_layout, this, true)
+        observeViewModel()
+    }
 
     //@formatter:off
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
         LayoutInflater.from(context).inflate(R.layout.color_picker_layout, this, true)
-        initViewModel()
+        observeViewModel()
     }
     //@formatter:on
 
+    private fun observeViewModel() {
+        val lockedAdapter =
+            LockedPickedColorAdapter(LayoutInflater.from(context)) { picked, action ->
+                when (action) {
+                    LockedPickedColorAdapter.Action.SELECT -> viewModel.setSelectedColor(picked)
+                    LockedPickedColorAdapter.Action.REMOVE -> viewModel.unlockColor(picked)
+                }
+            }
 
-    private fun initViewModel() {
+        with(colorPickerRecylerLocked) {
+            adapter = lockedAdapter
+        }
 
+        colorPickerImgLock.setOnClickListener {
+            viewModel.lockColor()
+        }
+
+        colorPickerSelectedColorView.setOnClickListener {
+            colorPickListener?.invoke(getPickedColor())
+        }
+
+        viewModel.lockedColorsLiveData.observe(lifecycleOwner, Observer {
+            lockedAdapter.submitList(it.toList())
+        })
+
+        viewModel.selectedColorLiveData.observe(lifecycleOwner, Observer {
+            val contrastColor = ColorUtils.calculateLuminance(it.intColor).let {
+                if (it < 0.5) {
+                    Color.WHITE
+                } else {
+                    Color.DKGRAY
+                }
+            }
+            colorPickerSelectedColorView.cast<CardView>().setCardBackgroundColor(it.intColor)
+            with(colorPickerSelectedColorText) {
+                setTextColor(contrastColor)
+                text = it.toString()
+            }
+            colorPickerImgLock.setColorFilter(contrastColor)
+
+        })
+        val accentColor = context.getColorCompat(R.color.secondaryColor)
+        viewModel.rChannelLiveData.observe(lifecycleOwner, Observer {
+            decorateSeekBar(colorPickerSeekR, accentColor, it, ColorPickerViewModel.R)
+        })
+        viewModel.gChannelLiveData.observe(lifecycleOwner, Observer {
+            decorateSeekBar(colorPickerSeekG, accentColor, it, ColorPickerViewModel.G)
+        })
+        viewModel.bChannelLiveData.observe(lifecycleOwner, Observer {
+            decorateSeekBar(colorPickerSeekB, accentColor, it, ColorPickerViewModel.B)
+        })
+        registerSeekListener(colorPickerSeekR, ColorPickerViewModel.R)
+        registerSeekListener(colorPickerSeekG, ColorPickerViewModel.G)
+        registerSeekListener(colorPickerSeekB, ColorPickerViewModel.B)
+    }
+
+    private fun decorateSeekBar(seekBar: SeekBar, foregroundColor: Int, channelColor: Int, channel: Int) {
+        val color = arrayOf(0, 0, 0).apply {
+            this[channel] = channelColor
+        }.let { Color.rgb(it[0], it[1], it[2]) }
+        with(seekBar) {
+            progress = channelColor
+            thumb.colorFilter = PorterDuff.Mode.SRC_ATOP.toColorFilter(
+                ColorUtils.compositeColors(
+                    ColorUtils.setAlphaComponent(color, 50),
+                    foregroundColor
+                )
+            )
+            progressDrawable.colorFilter = PorterDuff.Mode.SRC_ATOP.toColorFilter(color)
+        }
+    }
+
+    private fun registerSeekListener(seekBar: SeekBar, channel: Int) {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    viewModel.setChannelValue(progress, channel)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        return bundleOf(
+            State.STATE_KEY to State.serialized(
+                super.onSaveInstanceState(),
+                viewModel.selectedColorLiveData.value!!.intColor,
+                viewModel.lockedColorsLiveData.value!!.map { it.intColor }
+            )
+        )
+    }
+
+    override fun onRestoreInstanceState(restoredState: Parcelable?) {
+        if (restoredState is Bundle?) {
+            restoredState?.getParcelable<State>(State.STATE_KEY)?.let {
+                viewModel.restore(it.color, it.deserializeLockedColors())
+                super.onRestoreInstanceState(it.superState)
+            }
+        }
+        super.onRestoreInstanceState(BaseSavedState.EMPTY_STATE)
     }
 
     fun setOnColorPickListener(colorPickListener: (Int) -> Unit) {
         this.colorPickListener = colorPickListener
     }
 
-}
+    fun getPickedColor(): Int = viewModel.selectedColorLiveData.value!!.intColor
 
+    internal class State(superState: Parcelable?,
+                         val color: Int,
+                         val lockedColors: String) : BaseSavedState(superState) {
 
-internal class ColorPickerViewModel : ViewModel() {
+        companion object {
+            const val STATE_KEY = "ColorPickerState_Key"
+            private const val DELIM = ";"
+            fun serialized(superState: Parcelable?,
+                           selectedColor: Int,
+                           lockedColors: List<Int>): State =
+                State(superState, selectedColor, buildString {
+                    lockedColors.forEachIndexed { i, c ->
+                        val d = if (i < lockedColors.size - 1) DELIM else ""
+                        append("$c$d")
+                    }
+                })
+        }
 
-    companion object {
-        const val R = 0
-        const val G = 1
-        const val B = 2
-    }
-
-    private val selectionChannel: MutableLiveData<Pair<Int, Int>> = MutableLiveData()
-
-    val selectedColorLiveData: LiveData<Int> = selectionChannel.scan(0) { acc, curr ->
-        val channel = curr.second
-        val channelVal = curr.first
-        val rgb = arrayOf(Color.red(acc), Color.green(acc), Color.blue(acc))
-            .apply {
-                this[channel] = channelVal
+        fun deserializeLockedColors(): List<Int> = mutableListOf<Int>().apply {
+            lockedColors.split(DELIM).forEach {
+                add(it.toInt())
             }
-        Color.rgb(rgb[0], rgb[1], rgb[2])
+        }
     }
-
-    val rChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
-        Color.red(it)
-    }
-
-    val gChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
-        Color.green(it)
-    }
-
-    val bChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
-        Color.blue(it)
-    }
-
-    fun setChannelValue(value: Int, channel: Int) {
-        selectionChannel.value = value to channel
-    }
-
 
 }
+
+
---
+++ app/src/main/java/com/crskdev/biblereaderplus/presentation/common/colorpicker/ColorPickerViewModel.kt
@@ -0,0 +1,91 @@
+/*
+ * License: MIT
+ * Copyright (c)  Pela Cristian 2018.
+ */
+
+package com.crskdev.biblereaderplus.presentation.common.colorpicker
+
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.crskdev.biblereaderplus.common.util.cast
+import com.crskdev.biblereaderplus.presentation.util.arch.map
+
+internal class ColorPickerViewModel() : ViewModel() {
+
+    companion object {
+        const val R = 0
+        const val G = 1
+        const val B = 2
+        private val DEFAULT_PICKED = PickedColor(Color.BLACK)
+    }
+
+    val lockedColorsLiveData: LiveData<Set<PickedColor>> =
+        MutableLiveData<Set<PickedColor>>().apply {
+            value = emptySet()
+        }
+
+    val selectedColorLiveData: LiveData<PickedColor> = MutableLiveData<PickedColor>()
+        .apply {
+            value = DEFAULT_PICKED
+        }
+
+    val rChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
+        Color.red(it())
+    }
+
+    val gChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
+        Color.green(it())
+    }
+
+    val bChannelLiveData: LiveData<Int> = selectedColorLiveData.map {
+        Color.blue(it())
+    }
+
+    fun setChannelValue(value: Int, channel: Int) {
+        val prevColor = selectedColorLiveData.value ?: DEFAULT_PICKED
+        val rgb = mutableListOf(
+            Color.red(prevColor()),
+            Color.green(prevColor()),
+            Color.blue(prevColor())
+        ).apply {
+            this[channel] = value
+        }
+        selectedColorLiveData.cast<MutableLiveData<PickedColor>>().value = PickedColor(
+            Color.rgb(
+                rgb[0],
+                rgb[1],
+                rgb[2]
+            )
+        )
+    }
+
+    fun restore(selectedColor: Int, lockedColors: List<Int>) {
+        selectedColorLiveData.cast<MutableLiveData<PickedColor>>().value =
+                PickedColor(
+                    selectedColor
+                )
+        lockedColorsLiveData.cast<MutableLiveData<Set<PickedColor>>>().value = lockedColors.map {
+            PickedColor(it)
+        }.toSet()
+    }
+
+    fun lockColor() {
+        selectedColorLiveData.value?.let {
+            lockedColorsLiveData.cast<MutableLiveData<Set<PickedColor>>>()
+                .value = (lockedColorsLiveData.value ?: emptySet()) + it
+        }
+    }
+
+    fun unlockColor(color: PickedColor) {
+        lockedColorsLiveData.cast<MutableLiveData<Set<PickedColor>>>()
+            .value = (lockedColorsLiveData.value ?: emptySet()) - color
+    }
+
+    fun setSelectedColor(color: PickedColor) {
+        selectedColorLiveData.cast<MutableLiveData<PickedColor>>().value = color
+    }
+
+
+}
